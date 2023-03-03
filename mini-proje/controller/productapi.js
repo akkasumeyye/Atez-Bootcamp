d1e6c97f5a5cd4bcfba0862ca13cc729d4947c93
Sümeyye Akkaş <akkasumeyye@hotmail.com>
2023-03-03 20:21:32 +0300

    folder structer ve naming süzenlendi

--- mini-proje/controller/productapi.js
+++ mini-proje/controller/productapi.js
@@ -1,10 +1,43 @@
 "use strict";
 exports.__esModule = true;
-var IProductApi_1 = require("../services/IProductApi");
-var product = new IProductApi_1.ProductApi();
-console.log(product.findByCategory("tablet"));
-// console.log(product.findAll());
-// console.log(product.findByText("black"));
-// console.log(product.findCheapestProduct());
-// console.log(product.findStockCountMax20());
-// console.log(product.searchByBrandName("HP"));
+exports.ProductApi = void 0;
+var IProductService_1 = require("../services/IProductService");
+var ProductApi = /** @class */ (function () {
+    function ProductApi(service) {
+        this.service = service;
+    }
+    ProductApi.prototype.findAll = function () {
+        return this.service.findAll();
+    };
+    ProductApi.prototype.findByCategory = function (category) {
+        return this.service.findByCategory(category);
+    };
+    ProductApi.prototype.findByText = function (input) {
+        return this.service.findByText(input);
+    };
+    ;
+    //En ucuz ürünü seç
+    ProductApi.prototype.findCheapestProduct = function () {
+        return this.service.findCheapestProduct();
+    };
+    ;
+    //Stok 20 den az olan ürünleri getir
+    ProductApi.prototype.findStockCountMax20 = function () {
+        return this.service.findStockCountMax20();
+    };
+    ;
+    //marka ismiyle arama yap
+    ProductApi.prototype.searchByBrandName = function (brand) {
+        return this.service.searchByBrandName(brand);
+    };
+    ;
+    return ProductApi;
+}());
+exports.ProductApi = ProductApi;
+var productapi = new IProductService_1.ProductService();
+// console.log(productapi.findByCategory("tablet"));
+// console.log(productapi.findAll());
+console.log(productapi.findByText("black"));
+// console.log(productapi.findCheapestProduct());
+// console.log(productapi.findStockCountMax20());
+// console.log(productapi.searchByBrandName("HP"));
--- mini-proje/controller/productapi.ts
+++ mini-proje/controller/productapi.ts
@@ -1,9 +1,38 @@
 import { ProductService } from "../services/IProductService";
 
+export class ProductApi {
+    service:ProductService
+    constructor(service:ProductService) {
+        this.service = service;
+    }
+    
+  findAll () {
+    return this.service.findAll();
+  }
+  findByCategory (category: string) {
+    return this.service.findByCategory(category);
+  }
+  findByText (input: string) {
+    return this.service.findByText(input);
+  };
+  //En ucuz ürünü seç
+  findCheapestProduct () {
+    return this.service.findCheapestProduct();
+  };
+  //Stok 20 den az olan ürünleri getir
+  findStockCountMax20 () {
+    return this.service.findStockCountMax20()   
+  };
+  //marka ismiyle arama yap
+  searchByBrandName (brand: string) {
+    return this.service.searchByBrandName(brand);
+  };
+    
+}
 const productapi = new ProductService();
-console.log(productapi.findByCategory("tablet"));
+// console.log(productapi.findByCategory("tablet"));
 // console.log(productapi.findAll());
-// console.log(productapi.findByText("black"));
+console.log(productapi.findByText("black"));
 // console.log(productapi.findCheapestProduct());
 // console.log(productapi.findStockCountMax20());
 // console.log(productapi.searchByBrandName("HP"));
